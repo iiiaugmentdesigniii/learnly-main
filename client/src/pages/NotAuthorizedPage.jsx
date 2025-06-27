5c7c1d9776a5516e78517e6ad427cd1ec26438c2
iiblacksmithii <andrewsmith4@mail.weber.edu>
2025-06-26 19:53:48 -0600

    Not Sure What I changed

---
+++ client/src/pages/NotAuthorizedPage.jsx
@@ -0,0 +1,9 @@
+const NotAuthorizedPage = () => (
+	<div style={{ textAlign: "center", padding: "2rem" }}>
+		<h1>🚫 Not Authorized</h1>
+		<p>You do not have permission to view this page.</p>
+		<a href="/">Go back to Dashboard</a>
+	</div>
+);
+
+export default NotAuthorizedPage;
