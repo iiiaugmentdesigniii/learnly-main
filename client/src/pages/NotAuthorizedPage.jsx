1b13cbe7f29cb2cfcf1b8163d00454ca5e9eb33d
iiblacksmithii <andrewsmith4@mail.weber.edu>
2025-06-26 21:10:58 -0600

    final fix I hope

--- client/src/pages/NotAuthorizedPage.jsx
+++ client/src/pages/NotAuthorizedPage.jsx
@@ -2,7 +2,7 @@ const NotAuthorizedPage = () => (
 	<div style={{ textAlign: "center", padding: "2rem" }}>
 		<h1>🚫 Not Authorized</h1>
 		<p>You do not have permission to view this page.</p>
-		<a href="/login.jsx">Go back to Dashboard</a>
+		<a href="/login">Go back to Dashboard</a>
 	</div>
 );
 
